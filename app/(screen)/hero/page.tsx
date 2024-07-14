7be2f9c528764d21cefec96d3dd37142f847dfc8
Arodefarodef <umudefedora@gmail.com>
2024-07-14 05:16:52 -0700

    recomitting again again again

--- app/(screen)/hero/page.tsx
+++ app/(screen)/hero/page.tsx
@@ -1,16 +1,64 @@
+// import { url } from "inspector";
+import { redirect } from "next/navigation";
 import React from "react";
 
 const Page = async () => {
-  const url = "https://";
+  const url = "https://dumo-git-main-fedoras-projects-0d26f1b8.vercel.app/";
   const read = await fetch(url, { method: "GET", cache: "no-cache" });
   const data = await read.json();
-  const tru = data?.data?.length;
+  const check = data?.data?.length;
   // console.log(data);
+
+  const updateformAction = async (FormData: FormData) => {
+    "use server";
+    const title = FormData.get("title");
+    const desc = FormData.get("desc");
+    const image = FormData.get("image");
+    const color = FormData.get("color");
+    const fontSize = FormData.get("fontSize");
+    const heroID = FormData.get("heroID");
+
+    const Url = `https://dumo-git-main-fedoras-projects-0d26f1b8.vercel.app/${heroID}`;
+    await fetch(Url, {
+      method: "PATCH",
+      body: JSON.stringify({
+        title,
+        desc,
+        image,
+        color,
+        fontSize,
+      }),
+    });
+  };
+
+  const ActionForm = async (formData: FormData) => {
+    "use server";
+    const title = formData.get("title");
+    const desc = formData.get("desc");
+    const image = formData.get("image");
+    const color = formData.get("color");
+    const fontSize = formData.get("fontSize");
+
+    await fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        title,
+        desc,
+        image,
+        color,
+        fontSize,
+      }),
+    }).then(() => redirect("/here"));
+  };
+
   return (
     <div>
       <div className="flex justify-start pt-10 md:pt-1 md:justify-center items-center flex-col gap-3 h-[100vh] bg-purple-500 ml-20">
         <h1 className="font-semibold text-[18px]">Customize Your Hero Page</h1>
-        <form className="">
+        <form
+          action={check === 0 ? ActionForm : updateformAction}
+          className="w-[280px]  md:w-[400px] flex flex-col gap-3 border p-2 rounded-md"
+        >
           <div className="flex flex-col gap-2 p-1">
             <label className="font-semibold text-[15px]">Title</label>
             <div className="border grid md:grid-col-4 items-center pb-1">
@@ -20,6 +68,7 @@ const Page = async () => {
                 placeholder="Enter your hero title here"
                 name="title"
               />
+              <div className="col-span-1 md:col-span-3 gap-5 pr-1"></div>
             </div>
           </div>
         </form>
