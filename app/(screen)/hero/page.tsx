1a0f76b554797813e6489934e8cdbd1a75263732
Arodefarodef <umudefedora@gmail.com>
2024-07-14 06:29:24 -0700

    corrections

--- app/(screen)/hero/page.tsx
+++ app/(screen)/hero/page.tsx
@@ -3,11 +3,12 @@ import { redirect } from "next/navigation";
 import React from "react";
 
 const Page = async () => {
-  const url = "https://dumo-git-main-fedoras-projects-0d26f1b8.vercel.app/";
+  const url =
+    "https://dumo-git-main-fedoras-projects-0d26f1b8.vercel.app/api/here";
   const read = await fetch(url, { method: "GET", cache: "no-cache" });
   const data = await read.json();
   const check = data?.data?.length;
-  // console.log(data);
+  console.log(data);
 
   const updateformAction = async (FormData: FormData) => {
     "use server";
@@ -68,7 +69,32 @@ const Page = async () => {
                 placeholder="Enter your hero title here"
                 name="title"
               />
-              <div className="col-span-1 md:col-span-3 gap-5 pr-1"></div>
+              <div className="col-span-1 md:col-span-3 gap-5 pr-3 pl-1">
+                <div className="flex gap-4 items-center">
+                  <div className="flex gap-1 justify-center items-center">
+                    <span className="font-light text-[12px]">color:</span>
+                    <input
+                      defaultValue={data?.data[0]?.headercolor}
+                      className="w-[20px] h-[20px]"
+                      name="headercolor"
+                      type="color"
+                    />
+                  </div>
+
+                  <div className="flex gap-1 items-center">
+                    <span className="font-light text-[10px]">Font:</span>
+                    <select
+                      className="font-semibold text-[10px] outline-none border"
+                      name="headersize"
+                    >
+                      <option>10</option>
+                      <option>20</option>
+                      <option>30</option>
+                      <option>40</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </form>
